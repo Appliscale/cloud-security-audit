b1c753a5fbfd8ef27c314fb1f836f6ddc17c9a9c
sgargula <sylwia.gargula@wp.pl>
2019-01-17 18:03:02 +0100

    serverless.yml

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"github.com/Appliscale/cloud-security-audit/cmd"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func main() {
+type MyEvent struct {
+	Name string `json:"name"`
+}
+
+func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
 	cmd.Execute()
+	return fmt.Sprintf("Hello %s!", name.Name), nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
 }
